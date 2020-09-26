50488b66352b458c79094a6c3f7e9386f6654007
FranciscoValdesoiro <franciscovaldesoirogil@gmail.com>
2020-09-26 12:21:47 +0200

    Add shortid dependency, create phonelist and card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import PhoneListContainer from './components/PhoneListContainer';
 
 const App = () => {
 
@@ -9,9 +10,9 @@ const App = () => {
         <p>header</p>
         <Switch>
             <Route path="/" exact>
-                <p>phone list</p>
+                <PhoneListContainer />
             </Route>
-            <Route path="/details" exact>
+            <Route path="/detail/:id" exact>
                 <p>phone details</p>
             </Route>
         </Switch>
